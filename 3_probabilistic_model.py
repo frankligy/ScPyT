cd4d155b804e8ddea291489fb41054c7439f5890
Guangyuan(Frank) Li <frankli296@yahoo.com>
2023-03-06 10:25:01 -0500

    add probabilistic model

---
+++ 3_probabilistic_model.py
@@ -0,0 +1,102 @@
+import numpy as np
+from scipy.stats import norm,uniform,invgamma
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+norm.pdf(2,loc=0,scale=1)
+norm.cdf(0,loc=0,scale=1)
+norm.ppf(0.5,loc=0,scale=1)
+norm.sf(0,loc=0,scale=1)
+norm.isf(0.5,loc=0,scale=1)
+norm.rvs(size=100,loc=0,scale=1,random_state=1)
+
+# accept-reject method
+x = np.linspace(0,1,1000)
+f_x = x**1.7*((1-x)/(1+x))**5.3
+M = f_x.max()
+def f(x):
+    return x**1.7*((1-x)/(1+x))**5.3
+def g(x):
+    return uniform.pdf(x,loc=0,scale=1)
+
+n = 2500
+u = uniform.rvs(loc=0,scale=1,size=n,random_state=1)
+y = uniform.rvs(loc=0,scale=1,size=n,random_state=2)
+f_y = f(y)
+g_y = g(y)
+acc_or_rej = u <= f_y / (M * g_y)
+accepted_y = y[acc_or_rej]
+sns.histplot(accepted_y)
+
+# MCMC
+def f(x):
+    import math
+    return 2*x**2*(1-x)**8*math.cos(4*math.pi*x)**2
+def q(x,y):
+    return norm.pdf(x,loc=y,scale=0.1)
+
+n = 10000
+x = np.zeros(n)
+x[0] = norm.rvs(loc=0,scale=0.03,size=1)[0]
+for i in range(n-1):
+    while True:
+        x_cand = norm.rvs(loc=x[i],scale=0.03,size=1)[0]
+        if x_cand >= 0 and x_cand <= 1:
+            break
+    if x_cand >= 0 and x_cand <= 1:
+        rho = (q(x[i],x_cand)/q(x_cand,x[i]))*(f(x_cand)/f(x[i]))
+        alpha = min(1,rho)
+        u = uniform.rvs(loc=0,scale=1,size=1)[0]
+        if u < alpha:
+            x[i+1] = x_cand
+        else:
+            x[i+1] = x[i]
+sns.histplot(x)
+fig,ax = plt.subplots()
+ax.plot(np.arange(10000),x)
+
+# Gibbs sampling and Hierarchical bayesian
+nruns = 10000
+K = 100 # K = 100, K hospitals
+n = 1000 # each hospital has 1000 patients
+y = np.zeros(K,n)  # generated observations
+lambda_est = np.zeros(K,nruns)
+sigma_est = np.zeros(nruns)
+mu_est = np.zeros(nruns)
+tau_est = np.zeros(nruns)
+
+for i in range(K):
+    loc = uniform.rvs(loc=0,scale=10,size=1)[0]
+    scale = uniform.rvs(loc=0,scale=0.1,size=1)[0]
+    lambda_est[i,0] = norm.rvs(loc=loc,scale=scale,size=1)[0]
+sigma_est[0] = uniform.rvs(loc=0,scale=0.1,size=1)[0]
+mu_est[0] = norm.rvs(loc=uniform.rvs(loc=0,scale=10,size=1)[0],scale=1,size=1)[0]
+tau_est[0] = uniform.rvs(loc=0,scale=0.1,size=1)[0]
+
+for runs in range(1,n_runs-1,1):
+    # estimate lambda
+    for i in range(1,K-1):
+        mean = i/math.sqrt(1/tau_est[runs-1]) + n/sigma_est[runs-1]
+        std = (mean^2)*(mu_est[runs-1]/(tau_est[runs-1])+y[i,:].mean()*n/sigma_est[runs-1])
+        lambda_est[i,runs] = norm.rvs(loc=mean,scale=std,size=1)[0]
+    # estimate sigma
+    sigma_sum_term = 0
+    for i in range(K):
+        for j in range(n):
+            sigma_sum_term += (y[i,j]-lambda_est[i,runs])**2
+    sigma_est[runs] = invgamma(loc=K*n/2,scale=sigma_sum_term/2)
+    # estimate tau
+    tau_sum_term = 0
+    for i in range(K):
+        tau_sum_term += (lambda_est[i,runs]-mu_est[runs-1])**2
+    tau_est[runs] = invgamma(loc=K/2,scale=tau_sum_term/2)
+    # estimate mu
+    mu_est[runs] = norm.rvs(loc=lambda_est[:,runs-1].mean(),scale=math.sqrt(tau_est[runs]/2))
+
+
+n_cells = 1000
+n_genes = 100000
+PSI = np.empty(shape=(n_cells,n_genes))
+for i in rows:
+    for j in columns:
+        PSI[i,j] = pm.negativeBinomial(mean=..,scale=...)
---
+++ prob/dist_intuit.md
@@ -0,0 +1,85 @@
+
+**Simple distribution**
+
+# Beta
+
+Anything that are intrinsically bounded by 0 and 1. Emphasizing "intrinsical" is to set it apart from `logit_normal` distribution where it is also bounded by 0 and 1 due to the logi function. But it is more like a continuous random variable whereas a bound has been added.
+
+- You can use `Beta (s,s)` to represent distribution peaked at 0.5, `s` determine how informative this disitribution will be
+
+- You can use `Beta (0.5,0.5)` to represent bimodal distribution
+
+- When `a <= 1`, it is left skewed, when `b <= 1`, it is right skewed.
+
+# Gamma
+
+It is very flexible, anything that is positive can be modeled by Gamma, you just need to change the parameter to flexibly adjust the variance and shape. `chi2` distribution is a special case for Gamma. 
+
+- You can use `Gamma (m,1)` to make sure it is peaked at `m`
+
+- You can use `Gamma (s,s)` to make sure it is peaked at 1, and `s` determine how much regularisation you'd like to add on that when deviating from 1.
+
+# LogNorm
+
+It is to measure any positive values, without clear overdispersion.
+
+# Normal distribution
+
+Honestly the default, like to model the coefficent of the regression.
+
+# Poisson
+
+Default for counts data, the intuition is during certain time interval, how many buses arriving.
+
+# Exponential
+
+Related to the Poisson, the intuition is for the same situation in Poisson, the Exponential can measure the waiting time for the buses or the interval to be more general. It can be used for regularization as the probability goes down when the randome variable increases.
+
+# Negative Bionomial
+
+Poisson with overdispersion for counting data like RNA count, the intuition is, when achieving certain success, how many failures have been experienced.
+
+# Binomial
+
+Very intuitive, total number of trials each has a probability to succeed.
+
+# Bernoulli
+
+Very intuitive, toss the coin, a special case for binomial
+
+# Categorical
+
+Very intuitive, toss the dice, a special case for multinomial
+
+# Multinomial
+
+Very intuitive, toss n number of dice, the count of each side 
+
+# Dirichlet
+
+The multi-variate version of Beta, parameterizing Multinomial and categorical distribution
+
+# Wishard
+
+The multi-variate version of Gamme distribution.
+
+# HyperGeometric
+
+Very strong intuition, when having certain amount of fake product, and try to sample certain amount of product from total product.
+
+**Compound distribution**
+
+# Spike-and-Slab (or mixutre in general)
+
+A mixed distribution of a spike and a slab, useful for variable selection, a spike is a distribution that puts all mass into a center, whereas slab is a distribution that gives mass to all values.
+
+# Zero-inflated
+Add inflated zero component to like Poisson or Negative Binomial
+
+# Affine transformation
+
+The loc and scale parameter
+
+
+
+
